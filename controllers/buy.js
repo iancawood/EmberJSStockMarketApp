b8cf9da2a2b7daea786d4819230c581c27ea9626
Ian Wood <iwood@uwo.ca>
2015-03-08 20:45:47 -0400

    can now create orders

--- controllers/buy.js
+++ controllers/buy.js
@@ -2,7 +2,13 @@
 App.BuyController = Ember.Controller.extend({
     actions: {
         save: function(companyID){
-            console.log(companyID);
+            var company = this.store.find('company', companyID);
+            console.log(company);
+
+            this.store.createRecord('order', {
+                size: 11,
+                price: 10
+            });
         }
     }
 });
--- controllers/sell.js
+++ controllers/sell.js
@@ -2,7 +2,7 @@
 App.SellController = Ember.Controller.extend({
     actions: {
         save: function(){
-            
+
         }
     }
 });
